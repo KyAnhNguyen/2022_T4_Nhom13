c535118f9ef9673b8838374f46b5cab7fffe3a6a
KyAnhNguyen <19130011@st.hcmuaf.edu.vn>
2022-10-19 13:13:00 +0700

    update database

--- DataWarehouse/src/utils/DatabaseAttributes.java
+++ DataWarehouse/src/utils/DatabaseAttributes.java
@@ -2,5 +2,7 @@ package utils;
 
 public class DatabaseAttributes {
 	
-	public static final String STAGING = "staging";
+	public static final String STAGING_DATABASE = "staging";
+	public static final String DW_DATABASE = "datawarehouse";
+	public static final String CONTROLLER_DATABASE = "controller";
 }
--- DataWarehouse/src/utils/QUERIES.java
+++ DataWarehouse/src/utils/QUERIES.java
@@ -1,26 +1,30 @@
 package utils;
 
 public class QUERIES {
-	
+
 	// csv to staging
 	public static class QueryTransformCSV {
 		public static final String LOTTO = "";
 		public static final String PROVINCE = "";
 		public static final String PRIZE = "";
 	}
-	
+
 	// staging to data warehouse
 	public static class QueryTransformStaging {
 		public static final String LOTTO = "";
 		public static final String PROVINCE = "";
 		public static final String PRIZE = "";
 	}
-	
+
 	public static class QueryController {
 		public static final String INSERT_LOG = "";
 	}
-	
+
 	public static class Staging {
-		
+
+	}
+
+	public static class QueryConfig {
+		public static final String GET_DATABASE_STAGING = "select url from config where server_name = 'mysql' ";
 	}
 }
